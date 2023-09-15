9aea3ddb1309af6c20b31dd3941ace4761714fa3
cortelucas <corte1994.lc@gmail.com>
2023-09-15 20:47:37 -0300

    refactor: modularização

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Lucas S. Corte',
-    email: 'corte1994.lc@gmail.com',
-  },
-})
+app.register(appRoutes)
