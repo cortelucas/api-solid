d6afad4ed4b4ace586bb8a41007066250a2aa411
Lucas S. Corte <corte1994.lc@gmail.com>
2023-09-16 16:47:43 -0300

    test: ensures that a user is authenticated

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,24 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-user-repository'
+import { hash } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+import { AuthenticateUseCase } from './authenticate'
+
+describe('Authenticate use case', () => {
+  it('should be able to authenticate', async () => {
+    const userRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(userRepository)
+
+    await userRepository.create({
+      name: 'user_name',
+      email: 'any@email.com',
+      password_hash: await hash('any_password', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'any@email.com',
+      password: 'any_password',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+})
